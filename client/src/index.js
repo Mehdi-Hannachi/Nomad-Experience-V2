ff0c598ded2a3bcf261a6a96a5bc5ae3c92b8d00
Firas <mehdihannachi02@gmail.com>
2021-12-09 10:46:03 +0100

    Configure redux and connect store

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
@@ -9,10 +8,14 @@ import "assets/demo/demo.css?v=1.5.0";
 import "assets/demo/nucleo-icons-page-styles.css?v=1.5.0";
 
 import App from "App";
+import { Provider } from "react-redux";
+import store from "components/JS/store";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>,
   document.getElementById("root")
 );
